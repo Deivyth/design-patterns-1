ab82365321a389dfcd38a932c567b6a7d157a7f2
Deivyth Mena <driviti@hotmail.es>
2022-04-07 12:04:00 +0200

    Add function Undo

--- editor/src/main/java/com/kreitek/editor/ConsoleEditor.java
+++ editor/src/main/java/com/kreitek/editor/ConsoleEditor.java
@@ -1,6 +1,8 @@
 package com.kreitek.editor;
 
 import com.kreitek.editor.commands.CommandFactory;
+import com.kreitek.editor.commands.UndoCommand;
+import com.kreitek.editor.memento.CareTaker;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -17,7 +19,10 @@ public class ConsoleEditor implements Editor {
     public static final String TEXT_WHITE = "\u001B[37m";
 
     private final CommandFactory commandFactory = new CommandFactory();
-    private ArrayList<String> documentLines = new ArrayList<String>();
+    private final ArrayList<String> documentLines = new ArrayList<>();
+
+    private final UndoCommand undoCommand = new UndoCommand();
+    private final CareTaker commandCareTaker = CareTaker.getInstance();
 
     @Override
     public void run() {
@@ -26,7 +31,14 @@ public class ConsoleEditor implements Editor {
             String commandLine = waitForNewCommand();
             try {
                 Command command = commandFactory.getCommand(commandLine);
-                command.execute(documentLines);
+
+                if(!command.getClass().getSimpleName().equals("UndoCommand")){
+                    commandCareTaker.add(undoCommand.setState(documentLines));
+                    command.execute(documentLines);
+                }else{
+                    command.execute(documentLines);
+                }
+
             } catch (BadCommandException e) {
                 printErrorToConsole("Bad command");
             } catch (ExitException e) {
@@ -64,6 +76,7 @@ public class ConsoleEditor implements Editor {
         printLnToConsole("To add new line -> a \"your text\"");
         printLnToConsole("To update line  -> u [line number] \"your text\"");
         printLnToConsole("To delete line  -> d [line number]");
+        printLnToConsole("To undo command -> undo");
     }
 
     private void printErrorToConsole(String message) {
@@ -83,5 +96,7 @@ public class ConsoleEditor implements Editor {
     private void printToConsole(String message) {
         System.out.print(message);
     }
+
+
 
 }
